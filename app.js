bd49d0af78252df6abd6bcc3711a6e0ab12d668e
DESKTOP-88VMMCI\user <luzverde0314@gmail.com>
2022-03-02 00:59:33 +0900

    feat: 카카오페이지 백엔드 서버

---
+++ app.js
@@ -0,0 +1,30 @@
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const srcRouter = require("./routes/src");
+
+app.get("/", (req, res) => {
+  res.send('Hello World')
+})
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://127.0.0.1:3030");
+  next();
+})
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3030");
+  next();
+})
+
+app.use(
+  cookieParser(process.env.COOKIE_SECRET, { sameSite: "none", secure: true })
+);
+
+app.use("/src", srcRouter)
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`)
+})
