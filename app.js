1bf911c11a8ee2960cd2ad04b1daf8139f5aa1d3
DESKTOP-88VMMCI\user <luzverde0314@gmail.com>
2022-03-04 10:43:27 +0900

    feat: 헤로쿠 설정

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const cookieParser = require('cookie-parser');
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 8080;
 
 const srcRouter = require("./routes/src");
 
